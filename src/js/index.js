daf94770e9d54db9576ae6d4a489a58742397136
Пак Сергей <pakush1826@gmail.com>
2026-06-30 22:00:39 +0900

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,29 @@
 import '../scss/style.scss'
 import Swiper from 'swiper'
 import { Pagination } from 'swiper/modules'
-Swiper.use([Pagination])
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
 
-  // If we need pagination
+Swiper.use([Pagination])
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 16,
   pagination: {
-    el: '.swiper-pagination'
+    el: '.swiper-pagination',
+    clickable: true
   }
 });
+
+swiperRepair.use([Pagination])
+
+const swiperRepair = new Swiper('.swiper-repair', {
+  wrapperClass: 'swiper-wrapper-repair',
+  slideClass: 'swiper-slide__repair',
+
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+
+  pagination: {
+    el: '.swiper-pagination-repair',
+    clickable: true,
+  },
+})
