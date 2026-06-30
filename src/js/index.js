82d30ff5032ed2bbb6d3bcd884c1e5476990fc0a
Пак Сергей <pakush1826@gmail.com>
2026-06-30 19:43:14 +0900

    swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
 import '../scss/style.scss'
+import Swiper from 'swiper'
+import { Pagination } from 'swiper/modules'
+Swiper.use([Pagination])
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
 
-console.log('It works!')
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination'
+  }
+});
